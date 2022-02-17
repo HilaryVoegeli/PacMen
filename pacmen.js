02147bdb1d4f56bff54af8017426880d6a128188
Hilary Voegeli <94722702+HilaryVoegeli@users.noreply.github.com>
2022-02-16 19:00:22 -0600

    Update pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -5,12 +5,14 @@ const pacArray = [
 ];
 let direction = 0;
 const pacMen = [];
+
 function setToRandom(scale) {
   return {
     x: Math.random() * scale,
     y: Math.random() * scale,
   };
 }
+
 function makePac() {
   let velocity = setToRandom(10);
   let position = setToRandom(200);
@@ -27,6 +29,7 @@ function makePac() {
     newimg,
   };
 }
+
 function update() {
   pacMen.forEach((item) => {
     checkCollisions(item);
@@ -37,6 +40,7 @@ function update() {
   });
   setTimeout(update, 20);
 }
+
 function checkCollisions(item) {
   if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
   item.position.x < 0) {
@@ -46,9 +50,11 @@ function checkCollisions(item) {
     item.velocity.y = -item.velocity.y;
   }
 }
+
 function makeOne() {
   pacMen.push(makePac());
 }
+
 if (typeof module !== 'undefined') {
-  module.exports = { checkCollisions, update, pacMen };
+  module.exports = {checkCollisions, update, pacMen};
 }
