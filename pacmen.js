4a965e8c238c0bf5fea88c13bf46d76a900b02de
Hilary Voegeli <94722702+HilaryVoegeli@users.noreply.github.com>
2022-02-16 20:30:03 -0600

    Update pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -1,23 +1,25 @@
 let pos = 0;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ['./PacMan1.png', './PacMan2.png'],
+  ['./PacMan3.png', './PacMan4.png'],
 ];
 let direction = 0;
 const pacMen = [];
+
 function setToRandom(scale) {
   return {
     x: Math.random() * scale,
     y: Math.random() * scale,
   };
 }
+
 function makePac() {
   let velocity = setToRandom(10);
   let position = setToRandom(200);
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png';
+  newimg.src = './PacMan1.png';
   newimg.width = 100;
   pos = position;
   game.appendChild(newimg);
@@ -27,6 +29,7 @@ function makePac() {
     newimg,
   };
 }
+
 function update() {
   pacMen.forEach((item) => {
     checkCollisions(item);
@@ -37,6 +40,7 @@ function update() {
   });
   setTimeout(update, 20);
 }
+
 function checkCollisions(item) {
   if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
   item.position.x < 0) {
@@ -46,9 +50,11 @@ function checkCollisions(item) {
     item.velocity.y = -item.velocity.y;
   }
 }
+
 function makeOne() {
   pacMen.push(makePac());
 }
+
 if (typeof module !== 'undefined') {
   module.exports = { checkCollisions, update, pacMen };
 }
